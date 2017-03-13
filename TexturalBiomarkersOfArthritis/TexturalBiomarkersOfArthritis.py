5b3dfb3f20e67fd33fa55a5022722397b3343a4a
Jean-Baptiste VIMORT <jb.vimort@gmail.com>
2017-03-13 15:38:11 -0400

    ENH: Creation of a first version of the interface
    - Creation of the .ui file and loading of the file in the Scripted module
        - Creation of four main tabs: Input Data, Computation, Results, Exportation
        - First version of the input data tab
        - First version of the Computation tab

--- TexturalBiomarkersOfArthritis/TexturalBiomarkersOfArthritis.py
+++ TexturalBiomarkersOfArthritis/TexturalBiomarkersOfArthritis.py
@@ -1,70 +1,119 @@
-import os
-import unittest
-import vtk, qt, ctk, slicer
-from slicer.ScriptedLoadableModule import *
+import ctk
 import logging
+import os
+import qt
+import slicer
+import unittest
+import vtk
+from slicer.ScriptedLoadableModule import *
 
-
-#
-# TexturalBiomarkersOfArthritis
-#
+################################################################################
+#################  Textural Biomarkers of Arthritis ############################
+################################################################################
 
 
 class TexturalBiomarkersOfArthritis(ScriptedLoadableModule):
-
-  def __init__(self, parent):
-    ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "Textural Biomarkers Of Arthritis"
-    self.parent.categories = ["Examples"]
-    self.parent.dependencies = []
-    self.parent.contributors = ["Jean-Baptiste VIMORT (Kitware Inc..)"] # replace with "Firstname Lastname (Organization)"
-    self.parent.helpText = """
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "Textural Biomarkers Of Arthritis"
+        self.parent.categories = ["Examples"]
+        self.parent.dependencies = []
+        self.parent.contributors = ["Jean-Baptiste VIMORT (Kitware Inc..)"]
+        self.parent.helpText = """
     TODO
     """
-    self.parent.acknowledgementText = """
+        self.parent.acknowledgementText = """
     TODO
 """
 
-
-#
-# Textural Biomarkers of ArthritisWidget
-#
+################################################################################
+################  Textural Biomarkers of Arthritis Widget ######################
+################################################################################
 
 
 class TexturalBiomarkersOfArthritisWidget(ScriptedLoadableModuleWidget):
 
-  def setup(self):
-    ScriptedLoadableModuleWidget.setup(self)
-    print("-----  Textural Biomarkers of Arthritis widget setup -----")
+    # ************************************************************************ #
+    # -------------------------- Initialisation ------------------------------ #
+    # ************************************************************************ #
 
-    # Instantiate and connect widgets
+    def setup(self):
+        ScriptedLoadableModuleWidget.setup(self)
+        print("-----  Textural Biomarkers of Arthritis widget setup -----")
+        self.moduleName = 'TexturalBiomarkersOfArthritis'
+        scriptedModulesPath = eval('slicer.modules.%s.path' % self.moduleName.lower())
+        scriptedModulesPath = os.path.dirname(scriptedModulesPath)
+
+        # - Initialisation of Textural Biomarkers of Arthritis and its logic - #
+
+        self.logic = TexturalBiomarkersOfArthritisLogic()
+
+        # -------------------------------------------------------------------- #
+        # ----------------- Definition of the UI interface ------------------- #
+        # -------------------------------------------------------------------- #
+
+        # -------------------- Loading of the .ui file ----------------------- #
+
+        loader = qt.QUiLoader()
+        path = os.path.join(scriptedModulesPath, 'Resources', 'UI', '%s.ui' % self.moduleName)
+        print(path)
+        qfile = qt.QFile(path)
+        qfile.open(qt.QFile.ReadOnly)
+        widget = loader.load(qfile, self.parent)
+        print widget
+        self.layout = self.parent.layout()
+        self.widget = widget
+        self.layout.addWidget(widget)
+
+        # ******************************************************************** #
+        # ------------------------------ Algorithm --------------------------- #
+        # ******************************************************************** #
+
+    def cleanup(self):
+        pass
 
 
-  def cleanup(self):
-    pass
-
-
-#
-# TexturalBiomarkersOfArthritisLogic
-#
+################################################################################
+#################  Textural Biomarkers Of Arthritis Logic ######################
+################################################################################
 
 
 class TexturalBiomarkersOfArthritisLogic(ScriptedLoadableModuleLogic):
-  def __init__(self):
-    print "----- Textural Biomarkers of Arthritis logic init -----"
+
+    # ************************************************************************ #
+    # -------------------------- Initialisation ------------------------------ #
+    # ************************************************************************ #
+
+    def __init__(self):
+        print "----- Textural Biomarkers of Arthritis logic init -----"
+
+    # ************************************************************************ #
+    # ------------------------------ Algorithm ------------------------------- #
+    # ************************************************************************ #
+
+################################################################################
+#################  Textural Biomarkers Of Arthritis Test #######################
+################################################################################
 
 
 class TexturalBiomarkersOfArthritisTest(ScriptedLoadableModuleTest):
+    # ************************************************************************ #
+    # -------------------------- Initialisation ------------------------------ #
+    # ************************************************************************ #
 
-  def setUp(self):
-    print "----- Textural Biomarkers of Arthritis test setup -----"
-    # reset the state - clear scene
-    self.delayDisplay("Clear the scene")
-    slicer.mrmlScene.Clear(0)
+    def setUp(self):
+        print "----- Textural Biomarkers of Arthritis test setup -----"
+        # reset the state - clear scene
+        self.delayDisplay("Clear the scene")
+        slicer.mrmlScene.Clear(0)
 
-  def runTest(self):
-    self.setUp()
-    self.test_TexturalBiomarkersOfArthritis1()
+        # ******************************************************************** #
+        # ---------- Testing of Textural Biomarkers Of Arthritis ------------- #
+        # ******************************************************************** #
 
-  def test_TexturalBiomarkersOfArthritis1(self):
-    self.delayDisplay("Starting the test")
+    def runTest(self):
+        self.setUp()
+        self.test_TexturalBiomarkersOfArthritis1()
+
+    def test_TexturalBiomarkersOfArthritis1(self):
+        self.delayDisplay("Starting the test")
